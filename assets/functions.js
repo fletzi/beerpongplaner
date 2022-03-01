60b85752b861ec9446198943e1318c42031c75fe
fletzi <m.fletzer@icloud.com>
2022-03-01 16:44:18 +0100

    adjustments

--- assets/functions.js
+++ assets/functions.js
@@ -23,25 +23,44 @@ function validateLogin() {
 }
 
 function validateRegister() {
-    var username = document.getElementById("login_userName");
-    var password = document.getElementById("login_password");
-    var alert_null_un = document.getElementById("login_alert_un");
-    var alert_null_pw = document.getElementById("login_alert_pw");
+    var firstname = document.getElementById("reg_firstName");
+    var lastname = document.getElementById("reg_lastName");
+    var username = document.getElementById("reg_userName");
+    var password = document.getElementById("reg_password");
+
     var alert_fail = document.getElementById("login_alert_fail");
     var alert_success = document.getElementById("login_alert_success");
 
     alert_fail.hidden=true;
-    alert_null_un.hidden=true;
-    alert_null_pw.hidden=true;
     alert_success.hidden=true;
 
-    if(username.value == "") {
+    if(firstname.value == "") {
         alert_null_un.hidden=false;
-    } else if(password.value == "") {
+    } else if(lastname.value == "") {
         alert_null_pw.hidden=false;
-    } else {
-        window.location.href = "../pages/index.html";
-        //db con
-        //link to frontend backend
+    } else if(username.value == "") {
+
+    } else if(password.value == "") {
+
     }
+}
+
+function showPw() {
+    var btn_show = document.getElementById("button_sp")
+    var btn_hide = document.getElementById("button_sp_null")
+
+    document.getElementById("reg_password").type="text"
+    document.getElementById("reg_password2").type="text"
+    btn_show.hidden=true;
+    btn_hide.hidden=false;
+}
+
+function hidePw() {
+    var btn_show = document.getElementById("button_sp")
+    var btn_hide = document.getElementById("button_sp_null")
+
+    document.getElementById("reg_password").type="password"
+    document.getElementById("reg_password2").type="password"
+    btn_show.hidden=false;
+    btn_hide.hidden=true;
 }
