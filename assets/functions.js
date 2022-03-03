9fa24c8160c7b368faa1e1fb6d8e61ab124e1a71
fletzi <m.fletzer@icloud.com>
2022-03-03 12:35:31 +0100

    login/register adjustments

--- assets/functions.js
+++ assets/functions.js
@@ -16,6 +16,7 @@ function validateLogin() {
     } else if(password.value == "") {
         alert_null_pw.hidden=false;
     } else {
+        clearFields_login();
         window.location.href = "../pages/index.html";
         //db con
         //link to frontend backend
@@ -27,22 +28,51 @@ function validateRegister() {
     var lastname = document.getElementById("reg_lastName");
     var username = document.getElementById("reg_userName");
     var password = document.getElementById("reg_password");
+    var password2 = document.getElementById("reg_password2");
 
-    var alert_fail = document.getElementById("login_alert_fail");
-    var alert_success = document.getElementById("login_alert_success");
+    var alert_vn = document.getElementById("reg_alert_fn_null");
+    var alert_ln = document.getElementById("reg_alert_ln_null");
+    var alert_un = document.getElementById("reg_alert_un_null");
+    var alert_un2 = document.getElementById("reg_alert_un");
+    var alert_pw = document.getElementById("reg_alert_pw_null");
+    var alert_pw2 = document.getElementById("reg_alert_pw");
+    var alert_success = document.getElementById("reg_success");
 
-    alert_fail.hidden=true;
-    alert_success.hidden=true;
+    alert_vn.hidden=true;
+    alert_ln.hidden=true;
+    alert_un.hidden=true;
+    alert_pw.hidden=true;
+    alert_pw2.hidden=true;
 
-    if(firstname.value == "") {
-        alert_null_un.hidden=false;
+    if(username.value == "") {
+        alert_un.hidden=false;
+    } else if(!username.value) {
+        alert_un2.hidden=false;
+    } else if(firstname.value == "") {
+        alert_vn.hidden=false;
     } else if(lastname.value == "") {
-        alert_null_pw.hidden=false;
-    } else if(username.value == "") {
-
+        alert_ln.hidden=false;
     } else if(password.value == "") {
-
+        alert_pw.hidden=false;
+    } else if(password.value != password2.value) {
+        alert_pw2.hidden=false
+    } else {
+        alert_success.hidden=false;
+        clearFields_register();
     }
+}
+
+function clearFields_register() {
+    document.getElementById("reg_firstName").value="";
+    document.getElementById("reg_lastName").value="";
+    document.getElementById("reg_userName").value="";
+    document.getElementById("reg_password").value="";
+    document.getElementById("reg_password2").value="";
+}
+
+function clearFields_login() {
+    document.getElementById("login_userName").value="";
+    document.getElementById("login_password").value="";
 }
 
 function showPw() {
